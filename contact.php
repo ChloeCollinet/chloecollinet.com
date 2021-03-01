fb23538271bfa01002dfa1d205e35d1ae8c426b3
ChloeCollinet <79805460+ChloeCollinet@users.noreply.github.com>
2021-03-01 14:08:20 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'webdesign@chloecollinet.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = 'vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Impossible to load the "PHP Email Form" Library!');
